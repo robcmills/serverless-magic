a96b87508afc3975410ba9d095afca490eb9092f
Robert Mills <robcmills@gmail.com>
2020-04-12 22:33:08 -0700

    Replicate Sets from indexedDB into redux on App entry

--- react-app/src/hexproof/App/Router.tsx
+++ react-app/src/hexproof/App/Router.tsx
@@ -1,11 +1,16 @@
-import React, { Suspense } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { data } from 'hexproof/data';
 import { LoadingScreen } from 'hexproof/components/LoadingScreen';
 import { DataRoute } from 'hexproof/routes/DataRoute';
 import { RootRoute } from 'hexproof/routes/RootRoute';
 
 export function Router() {
+	useEffect(() => {
+		data.replicateSets();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Suspense fallback={<LoadingScreen />}>
--- react-app/src/hexproof/data/index.tsx
+++ react-app/src/hexproof/data/index.tsx
@@ -1,10 +1,12 @@
-import { ISet } from 'hexproof/types/ISet';
 import { databaseService } from 'hexproof/database/service';
+import { hydrateSetsAction } from 'hexproof/redux/sets/actions';
+import { ISet } from 'hexproof/types/ISet';
 import { scryfallService } from 'hexproof/scryfall/service';
 
 interface IData {
 	downloadSets: () => Promise<ISet[]>;
 	getLocalSetsCount: () => Promise<number>;
+	replicateSets: () => Promise<void>;
 }
 
 const downloadSets = async () => {
@@ -13,7 +15,13 @@ const downloadSets = async () => {
 	return sets;
 }
 
+const replicateSets = async () => {
+	const sets: ISet[] = await databaseService.getAllSets();
+	hydrateSetsAction(sets);
+}
+
 export const data: IData = {
 	downloadSets,
 	getLocalSetsCount: databaseService.getLocalSetsCount,
+	replicateSets,
 }
