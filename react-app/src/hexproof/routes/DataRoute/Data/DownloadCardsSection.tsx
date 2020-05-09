873f2aa8c2cfffe68b8b8e2c1e4092945d1e851f
Robert Mills <robcmills@gmail.com>
2020-05-09 15:11:46 -0700

    Disable non-supported bulk downloads

--- react-app/src/hexproof/routes/DataRoute/Data/DownloadCardsSection.tsx
+++ react-app/src/hexproof/routes/DataRoute/Data/DownloadCardsSection.tsx
@@ -14,14 +14,30 @@ import {
 import { Button } from 'hexproof/components/Button';
 import { H2 } from 'hexproof/components/typography/H2';
 import { P } from 'hexproof/components/typography/P';
+import { Spinner } from 'hexproof/components/Spinner';
 
 import { SPACING_UNIT } from 'hexproof/styles/constants';
 
 const useStyles = createUseStyles({
+  button: {
+    textAlign: 'left',
+  },
   description: {
     margin: [SPACING_UNIT / 2, 0],
   },
+  spinner: {
+    margin: SPACING_UNIT,
+  },
+  disabled: {
+    fontSize: '14px',
+    fontStyle: 'italic',
+    opacity: 0.75,
+  },
 });
+
+const ENABLED_BULK_OBJECTS = [
+  'oracle_cards',
+];
 
 export function DownloadCardsSection() {
   const s = useStyles();
@@ -37,23 +53,32 @@ export function DownloadCardsSection() {
   }, [isDownloaded, isDownloadingBulkData]);
 
   const bulkDataButtons = bulkDataObjects.map((bulkObject: IScryFallBulkDataObject) => {
+    const isDisabled = !ENABLED_BULK_OBJECTS.includes(bulkObject.type);
     const megabyteSize = (bulkObject.compressed_size / 1000000).toFixed(2) + 'MB';
     return (
       <div key={bulkObject.id}>
-        <Button>
-          {`Download ${bulkObject.name} ~ ${megabyteSize}`}
+        <Button className={s.button} isDisabled={isDisabled}>
+          {`Download ${bulkObject.name}`} <br/>
+          {megabyteSize}
         </Button>
-        <P className={s.description}>{bulkObject.description}</P>
+        {isDisabled && <P className={s.disabled}>Not supported yet</P>}
+        <P className={s.description}>
+          {bulkObject.description}
+        </P>
       </div>
     );
   });
 
   const bulkDataSection = (
     <>
-      <H2>Download Cards</H2>
+      <H2>Download Bulk Data</H2>
       {bulkDataButtons}
     </>
   );
+
+  if (isDownloadingBulkData) {
+    return <Spinner className={s.spinner} />;
+  }
 
   return (
     <>
