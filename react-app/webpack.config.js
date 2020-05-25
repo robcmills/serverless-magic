4a12b6aa2b15f8bfcb0511c9e71b2734fde9a8c0
Robert Mills <robcmills@gmail.com>
2020-05-24 22:11:02 -0700

    Add clean webpack plugin

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -37,6 +38,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
